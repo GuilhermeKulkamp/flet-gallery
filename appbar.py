079e29cd15dcf579928b6c5c41f831b75acccc8d
GuilhermeKulkamp <guilhermekulkamp@gmail.com>
2024-10-15 22:09:36 -0300

    atualização de appbar

--- appbar.py
+++ appbar.py
@@ -1,7 +1,7 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page .bgcolor = ft.colors.BLACK87
+    page.bgcolor = ft.colors.BLUE
     page.theme_mode = ft.ThemeMode.DARK
     page.title = 'Navegações'
     page.window_width = 450
@@ -11,18 +11,27 @@ def main(page: ft.Page):
     page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.ADD,bgcolor=ft.colors.BLUE_ACCENT_700)
     page.floating_action_button_location = ft.FloatingActionButtonLocation.CENTER_DOCKED
 
+    def btn_main(e):
+        _stack_main.controls.clear()
+        _stack_main.controls.append(_main)
+        _stack_main.update()
+
+    def btn_edit(e):
+        _stack_main.controls.clear()
+        _stack_main.controls.append(editar)
+        _stack_main.update()
+
     page.bottom_appbar = ft.BottomAppBar(
         bgcolor=ft.colors.WHITE70,
         shape=ft.NotchShape.CIRCULAR,
         content=ft.Row(
             controls=[
-                ft.IconButton(icon=ft.icons.EDIT, icon_color=ft.colors.BLUE_ACCENT,icon_size=32),
+                ft.IconButton(icon=ft.icons.HOME, icon_color=ft.colors.BLUE_ACCENT,icon_size=32, on_click=btn_main),
                 ft.IconButton(icon=ft.icons.SEARCH, icon_color=ft.colors.BLUE_ACCENT,icon_size=32),
                 ft.Container(expand=True),
-                ft.IconButton(icon=ft.icons.SHARE, icon_color=ft.colors.BLUE_ACCENT,icon_size=32),
-                ft.IconButton(icon=ft.icons.SETTINGS, icon_color=ft.colors.BLUE_ACCENT,icon_size=32),
-            ]
-      
+                ft.IconButton(icon=ft.icons.EDIT, icon_color=ft.colors.BLUE_ACCENT,icon_size=32,on_click=btn_edit),
+                ft.IconButton(icon=ft.icons.DELETE, icon_color=ft.colors.BLUE_ACCENT,icon_size=32),
+            ]    
         )
     )
 
@@ -30,17 +39,38 @@ def main(page: ft.Page):
     _main = ft.Container(
         width=400,
         height=600,
+        alignment=ft.alignment.center,
         bgcolor=ft.colors.WHITE70,
         border_radius=16,
+        shadow=ft.BoxShadow(blur_radius=10,color=ft.colors.with_opacity(opacity=0.5,color=ft.colors.BLACK)),
+        content= ft.Text(
+            value="INÍCIO",
+            color=ft.colors.BLACK,
+            size=32,
+        )
+    )
+
+    #container editar
+    editar = ft.Container(
+        width=400,
+        height=600,
+        alignment=ft.alignment.center,
+        bgcolor=ft.colors.WHITE70,
+        border_radius=16,
+        shadow=ft.BoxShadow(blur_radius=10,color=ft.colors.with_opacity(opacity=0.5,color=ft.colors.BLACK)),
+        content= ft.Text(
+            value="EDITAR",
+            color=ft.colors.BLACK,
+            size=32,
+        )
     )
 
     #stack principal
     _stack_main = ft.Stack(
         controls=[
-            _main
+            _main,
         ]
         )
-    
 
     page.add(_stack_main)
 
